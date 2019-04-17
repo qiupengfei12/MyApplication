b65b49dd6233fcedbcfd31645fd2182138f09e30
qiupengfei12 <17600610524@163.com>
2019-04-17 16:41:02 +0800

    dd

--- app/src/main/java/com/example/bx/myapplication/fragment/Fragment1.java
+++ app/src/main/java/com/example/bx/myapplication/fragment/Fragment1.java
@@ -4,14 +4,30 @@ package com.example.bx.myapplication.fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.bx.myapplication.R;
+import com.example.bx.myapplication.View.PinnedHeaderListView;
+import com.example.bx.myapplication.adapter.LeftListAdapter;
+import com.example.bx.myapplication.adapter.MainSectionedAdapter;
 
 public class Fragment1 extends BaseFragment {
 
+    ListView leftListview;
+    PinnedHeaderListView pinnedListView;
+    private boolean isScroll = true;
+    private LeftListAdapter adapter;
+    private String[] leftStr = new String[]{"面食类", "盖饭", "寿司", "烧烤", "酒水", "凉菜", "小吃", "粥", "休闲"};
+    private boolean[] flagArray = {true, false, false, false, false, false, false, false, false};
+    private String[][] rightStr = new String[][]{{"热干面", "臊子面", "烩面"},
+            {"番茄鸡蛋", "红烧排骨", "农家小炒肉"},
+            {"芝士", "丑小丫", "金枪鱼"}, {"羊肉串", "烤鸡翅", "烤羊排"}, {"长城干红", "燕京鲜啤", "青岛鲜啤"},
+            {"拌粉丝", "大拌菜", "菠菜花生"}, {"小食组", "紫薯"},
+            {"小米粥", "大米粥", "南瓜粥", "玉米粥", "紫米粥"}, {"儿童小汽车", "悠悠球", "熊大", " 熊二", "光头强"}
+    };
 
-    private TextView tesssww;
 
     @Override
     protected int setLayoutId() {
@@ -21,14 +37,91 @@ public class Fragment1 extends BaseFragment {
     //控件的部署
     @Override
     protected void init(View view, Bundle savedInstanceState) {
-        tesssww = (TextView) view.findViewById(R.id.tesssww);
-        tesssww.setOnClickListener(new View.OnClickListener() {
+        leftListview = (ListView)view.findViewById(R.id.left_listview);
+        pinnedListView = (PinnedHeaderListView)view.findViewById(R.id.pinnedListView);
+        final MainSectionedAdapter sectionedAdapter = new MainSectionedAdapter(getActivity(), leftStr, rightStr);
+        pinnedListView.setAdapter(sectionedAdapter);
+        adapter = new LeftListAdapter(getActivity(), leftStr, flagArray);
+        leftListview.setAdapter(adapter);
+        leftListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
-            public void onClick(View view) {
-                tesssww.setText("444141414");
+            public void onItemClick(AdapterView<?> arg0, View view, int position, long arg3) {
+                isScroll = false;
+
+                for (int i = 0; i < leftStr.length; i++) {
+                    if (i == position) {
+                        flagArray[i] = true;
+                    } else {
+                        flagArray[i] = false;
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                int rightSection = 0;
+                for (int i = 0; i < position; i++) {
+                    rightSection += sectionedAdapter.getCountForSection(i) + 1;
+                }
+                pinnedListView.setSelection(rightSection);
+
             }
+
         });
 
+        pinnedListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView arg0, int scrollState) {
+                switch (scrollState) {
+                    // 当不滚动时
+                    case AbsListView.OnScrollListener.SCROLL_STATE_IDLE:
+                        // 判断滚动到底部
+                        if (pinnedListView.getLastVisiblePosition() == (pinnedListView.getCount() - 1)) {
+                            leftListview.setSelection(ListView.FOCUS_DOWN);
+                        }
+
+                        // 判断滚动到顶部
+                        if (pinnedListView.getFirstVisiblePosition() == 0) {
+                            leftListview.setSelection(0);
+                        }
+
+                        break;
+                }
+            }
+
+            int y = 0;
+            int x = 0;
+            int z = 0;
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (isScroll) {
+                    for (int i = 0; i < rightStr.length; i++) {
+                        if (i == sectionedAdapter.getSectionForPosition(pinnedListView.getFirstVisiblePosition())) {
+                            flagArray[i] = true;
+                            x = i;
+                        } else {
+                            flagArray[i] = false;
+                        }
+                    }
+                    if (x != y) {
+                        adapter.notifyDataSetChanged();
+                        y = x;
+                        if (y == leftListview.getLastVisiblePosition()) {
+//                            z = z + 3;
+                            leftListview.setSelection(z);
+                        }
+                        if (x == leftListview.getFirstVisiblePosition()) {
+//                            z = z - 1;
+                            leftListview.setSelection(z);
+                        }
+                        if (firstVisibleItem + visibleItemCount == totalItemCount - 1) {
+                            leftListview.setSelection(ListView.FOCUS_DOWN);
+                        }
+                    }
+                } else {
+                    isScroll = true;
+                }
+            }
+        });
 
     }
 
